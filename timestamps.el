04436f2bd621a8d592f446cdbdf293b37e867843
Marshall Van Loon <marshall.vanloon@cfacorp.com>
2020-10-16 12:58:06 -0400

    add timestamp functions

--- timestamps.el
+++ timestamps.el
@@ -1,10 +1,13 @@
 (defun insert-date ()
   "Inserts ISO8601 date."
+  (interactive)
   (insert (format-time-string "%Y-%m-%d")))
 
 (global-set-key (kbd "C-c d") 'insert-date)
 
 (defun insert-datetime ()
+  "Inserts ISO8601 datetime."
+  (interactive)
   (insert (format-time-string "%Y-%m-%dT%H:%M:%S%Z")))
 
 (global-set-key (kbd "C-c t") 'insert-datetime)
